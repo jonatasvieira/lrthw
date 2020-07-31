36b29dd43981f206a2721dd9eef746f12f39f27f
Jonatas Vieira <jsoovieira@gmail.com>
2020-07-31 13:41:35 -0300

    Initial commit.

---
+++ ex2.rb
@@ -0,0 +1,15 @@
+# a comment, this is so you can read your program later.
+
+#Anything after the # is ignored by ruby
+
+puts "I Could have code like this." # and the comment after is ignored
+
+#You can also use a comment to "disable" or comment out a pice of code:
+
+# puts "This won't run."
+
+puts "This will run."
+
+5.times { |index|
+    puts "Hi #{index} there."
+}
